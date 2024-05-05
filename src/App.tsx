3479b7ab044813a6d1e5d5c0fca8a17fa21be3b4
AbhishekRavula <ravulaabhishek@gmail.com>
2024-05-05 11:38:53 +0530

    layout for jobs list and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Vite React Web App</div>;
+  const [jobsList, setJobsList] = useState(new Array(20).fill("job"));
+
+  return (
+    <div className="body-container">
+      <div className="job-filters-container">Job Filters Sections</div>
+      <div className="jobs-list-container">
+        {jobsList.map((job, index) => {
+          return (
+            <div key={index} className="job-item">
+              {job} item {index + 1}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
